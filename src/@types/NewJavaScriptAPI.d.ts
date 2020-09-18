d6efb955803c590218c46d6ffe581abbb3143292
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-18 18:04:36 +0900

    fix configs

--- src/@types/NewJavaScriptAPI.d.ts
+++ src/@types/NewJavaScriptAPI.d.ts
@@ -1,3 +1,10 @@
 interface MediaDevices {
   getDisplayMedia: (MediaStreamConstraints) => MediaStream
 }
+
+interface MediaTrackConstraints {
+  mandatory: {
+    chromeMediaSource?: 'desktop',
+    chromeMediaSourceId?: string
+  }
+}
---
+++ src/@types/Original.ts
@@ -0,0 +1,9 @@
+export interface URI {
+  addr: string
+  port: number
+}
+
+export interface Setting {
+  privateIP: string,
+  browserPort: number,
+}
