4e1e5e687e438a8c11bd2b4371e619f04a4447fe
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-10-07 00:47:46 +0900

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,10 @@
 	import Router from 'svelte-spa-router'
 	import Desktop from './components/Desktop/Desktop.svelte'
 	import Select from './components/Desktop/SelectWindowDialog.svelte'
-	import Tablet from './components/Tablet/Tablet.svelte'
-	import TabletSettingLayout from './components/Tablet/TabletSettingLayout.svelte'
-	import TabletSettingSort from './components/Tablet/TabletSettingSort.svelte';
-	import TabletSettingTemplate from './components/Tablet/TabletSettingTemplate.svelte';
 
 	const routes = {
 		'/': Desktop,
 		'/select': Select,
-		'/client': Tablet,
-		'/client/setting/layout': TabletSettingLayout,
-		'/client/setting/template': TabletSettingTemplate,
-		'/client/setting/sort': TabletSettingSort,
 	}
 	onMount(() => {
 	})
