34af63166ffecc0f039dfb8f3175bc90ff1d1405
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-10-17 06:59:57 +0900

    Revert "fix hangup"
    This reverts commit 524d434e0c13dfc914339d20db7da333f0016367.

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,11 +9,10 @@ export const store = {
   ws: writable<WebSocket | null>(null),
   isSetAnswer: writable(false),
   dataChannel: writable<RTCDataChannel | null>(null),
-  movieChannel: writable<RTCDataChannel | null>(null),
 
   candidates: writable<{ id: string, name: string, url: string }[]>([]),
 
   chunks: writable<Blob[]>([]),
   recorder: writable<MediaRecorder | null>(null),
-  me: writable("originala")
+  me: writable("")
 }
