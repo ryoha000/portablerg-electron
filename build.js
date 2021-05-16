a702b13660a62a78d5721420abf6140d38ef0ea2
ryoha000 <the7uya@gmail.com>
2021-05-16 17:42:10 +0900

    appxを吐くように

--- build.js
+++ build.js
@@ -1,9 +1,32 @@
-const { build } = require('electron-builder');
+import builder from 'electron-builder'
+import process from 'process'
+import dotenv from 'dotenv'
 
-build({
-  config: {
-    appId: 'portablerg',
-    productName: 'portablerg',
-    icon: 'public/favicon.png'
-  },
+dotenv.config();
+
+const identityName = process.env.IDENTITY_NAME
+const applicationId = process.env.APPLICATION_ID
+const publisherDisplayName = process.env.PUBLISHER_DISPLAY_NAME
+const publisher = process.env.PUBLISHER
+const languages = ['JA-JP', 'EN-US']
+
+if (!(identityName && applicationId && publisherDisplayName && publisher)) {
+	console.error('env is missing. required: IDENTITY_NAME, APPLICATION_ID, PUBLISHER_DISPLAY_NAME, and PUBLISHER')
+	process.exit()
+}
+
+builder.build({
+	config: {
+		appId: 'portablerg',
+		productName: 'portablerg',
+		icon: 'public/favicon.png',
+		appx: {
+			identityName,
+			applicationId,
+			publisherDisplayName,
+			publisher,
+			languages
+		},
+		win: { 'target': 'appx' }
+	},
 });
