196277cbe93a4cc2c6cd0d9503d42a79f76026ec
ryoha000 <the7uya@gmail.com>
2021-07-06 19:44:06 +0900

    fix SelectWindowDialog

--- src/components/Desktop/SelectWindowDialog.svelte
+++ src/components/Desktop/SelectWindowDialog.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import SelectWindowDialogItem from './SelectWindowDialogItem.svelte'
-  import { getSources, decideWindow } from '../../renderLogic'
-  import { store } from '../../store'
+  import { onMount } from "svelte";
+  import SelectWindowDialogItem from "./SelectWindowDialogItem.svelte";
+  import { getSources, decideWindow } from "../../renderLogic";
+  import { store } from "../../store";
 
-  let candidates: { id: string, name: string, url: string }[] = []
-  store.candidates.subscribe(v => candidates = v )
-  const select = (e: CustomEvent<{ id: string, name: string }>) => {
-    decideWindow(e.detail.id, e.detail.name)
-  }
+  let candidates: { id: string; name: string; url: string }[] = [];
+  store.candidates.subscribe((v) => (candidates = v));
+  const select = (e: CustomEvent<{ id: string; name: string }>) => {
+    decideWindow(e.detail.id, e.detail.name);
+  };
   onMount(() => {
     getSources((e) => {
-      candidates = e
-    })
-  })
+      candidates = e;
+    });
+  });
 </script>
-
 
 {#if candidates.length !== 0}
   <div>
     <style>
       .rootContainer {
-        display: flex;
-        flex-wrap: wrap;
+        grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
         padding: 24px;
+        gap: 16px;
+        display: grid;
       }
     </style>
     <div class="rootContainer">
       {#each candidates as candidate}
-        <SelectWindowDialogItem on:select="{select}" {candidate} />
+        <SelectWindowDialogItem on:select={select} {candidate} />
       {/each}
     </div>
   </div>
